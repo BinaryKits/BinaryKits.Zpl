ef3be01b7c053164370e1a5092b647438c45fb56
Yiping <Yiping.Ruan@outlook.com>
2017-04-20 13:57:27 +0800

    Update test

--- ZPLUtilityUnitTest/BasicTests.cs
+++ ZPLUtilityUnitTest/BasicTests.cs
@@ -27,10 +27,25 @@ namespace ZPLUtilityUnitTest
             labelElements.Add(new ZPLGraphicCircle(400, 700, 100, 5));
             labelElements.Add(new ZPLGraphicDiagonalLine(400, 700, 100, 50, 5));
             labelElements.Add(new ZPLGraphicDiagonalLine(400, 700, 50, 100, 5));
-            labelElements.Add(new ZPLGraphicSymbol(ZPLGraphicSymbol.GraphicSymbolCharacter.RegisteredTradeMark, 600, 600, 50, 50));
+            labelElements.Add(new ZPLGraphicSymbol(ZPLGraphicSymbol.GraphicSymbolCharacter.Copyright, 600, 600, 50, 50));
+
+            //Add raw ZPL code
+            labelElements.Add(new ZPLRaw("^FO200, 200^GB300, 200, 10 ^FS"));
 
             var renderEngine = new ZPLEngine(labelElements);
-            var output = renderEngine.ToZPLString(new ZPLRenderOptions() { DisplayComments = true, AddEmptyLineBeforeElementStart = true });
+            var output = renderEngine.ToZPLString(new ZPLRenderOptions() { AddEmptyLineBeforeElementStart = true });
+
+            Console.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void ChangeDPI()
+        {
+            var labelElements = new List<ZPLElementBase>();
+            labelElements.Add(new ZPLGraphicBox(400, 700, 100, 100, 5));
+
+            var options = new ZPLRenderOptions() { SourcePrintDPI = 203, TargetPrintDPI = 300 };
+            var output = new ZPLEngine(labelElements).ToZPLString(options);
 
             Console.WriteLine(output);
         }
