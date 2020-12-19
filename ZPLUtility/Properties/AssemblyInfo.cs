e75179448af307fa478e614e2bf50d75d46bfa05
Ruan Yiping <yiping.ruan@outlook.com>
2020-12-19 18:13:54 +0800

    Update version

--- ZPLUtility/Properties/AssemblyInfo.cs
+++ ZPLUtility/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Binary Kits Pte. Ltd.")]
 [assembly: AssemblyProduct("ZPLUtility")]
-[assembly: AssemblyCopyright("BinaryKits Pte. Ltd.  2017")]
+[assembly: AssemblyCopyright("BinaryKits Pte. Ltd.  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
