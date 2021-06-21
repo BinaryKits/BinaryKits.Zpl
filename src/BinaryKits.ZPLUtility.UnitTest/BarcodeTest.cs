42f9f75985eedea22995a7b5aa3cefac63243872
Tino Hager <tino.hager@nager.at>
2021-06-21 22:17:13 +0200

    Add ZPLBarcodeEan13
    https://github.com/BinaryKits/ZPLUtility/issues/14

--- src/BinaryKits.ZPLUtility.UnitTest/BarcodeTest.cs
+++ src/BinaryKits.ZPLUtility.UnitTest/BarcodeTest.cs
@@ -40,5 +40,21 @@ namespace ZPLUtility.UnitTest
             Assert.IsNotNull(output);
             Assert.AreEqual("^XA\n^LH0,0\n^CI28\n\n^FO100,300\n^BN,100,Y,N\n^FD123ABC^FS\n^XZ", output);
         }
+
+        [TestMethod]
+        public void BarcodeEan13()
+        {
+            var elements = new List<ZPLElementBase>
+            {
+                new ZPLBarcodeEan13("123456", 100, 300)
+            };
+
+            var renderEngine = new ZPLEngine(elements);
+            var output = renderEngine.ToZPLString(new ZPLRenderOptions { AddEmptyLineBeforeElementStart = true });
+
+            Debug.WriteLine(output);
+            Assert.IsNotNull(output);
+            Assert.AreEqual("^XA\n^LH0,0\n^CI28\n\n^FO100,300\n^BEN,100,Y,N\n^FD123456^FS\n^XZ", output);
+        }
     }
 }
---
+++ src/BinaryKits.ZPLUtility/Elements/ZPLBarcodeEan13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryKits.Utility.ZPLUtility.Elements
+{
+    /// <summary>
+    /// EAN-13
+    /// </summary>
+    public class ZPLBarcodeEan13 : ZPLBarcode
+    {
+        public ZPLBarcodeEan13(string content, int positionX, int positionY, int height = 100, string orientation = "N", bool printInterpretationLine = true, bool printInterpretationLineAboveCode = false)
+            : base(content, positionX, positionY, height, orientation, printInterpretationLine, printInterpretationLineAboveCode)
+        {
+            if (!IsDigitsOnly(content))
+            {
+                throw new ArgumentException("EAN-13 Barcode allow only digits", nameof(content));
+            }
+        }
+
+        public override IEnumerable<string> Render(ZPLRenderOptions context)
+        {
+            var result = new List<string>();
+            result.AddRange(Origin.Render(context));
+            result.Add($"^BE{Orientation},{context.Scale(Height)},{(PrintInterpretationLine ? "Y" : "N")},{(PrintInterpretationLineAboveCode ? "Y" : "N")}");
+            result.Add($"^FD{Content}^FS");
+
+            return result;
+        }
+    }
+}
