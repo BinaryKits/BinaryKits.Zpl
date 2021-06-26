bbfc51dc4d13b2b02a31e26e622b8f4766857dde
Ruan Yiping <yiping.ruan@outlook.com>
2021-06-26 15:02:40 +0800

    Change namespace BinaryKits.ZplUtility.TestConsole.cs

--- src/BinaryKits.ZplUtility.TestConsole/LabelaryApiClient.cs
+++ src/BinaryKits.ZplUtility.TestConsole/LabelaryApiClient.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BinaryKits.ZplUtility.TestConsole.cs
+namespace BinaryKits.ZplUtility.TestConsole
 {
     public class LabelaryApiClient
     {
