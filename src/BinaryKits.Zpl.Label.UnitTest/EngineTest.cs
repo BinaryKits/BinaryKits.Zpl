2fe4454a8defd0f1cff9c3c45ac888aa3360ea33
Ruan Yiping <yiping.ruan@outlook.com>
2021-07-14 11:56:01 +0800

    Rename project "BinaryKits.ZplUtility.UnitTest" to "BinaryKits.Zpl.Label.UnitTest"

--- src/BinaryKits.Zpl.Label.UnitTest/EngineTest.cs
+++ src/BinaryKits.Zpl.Label.UnitTest/EngineTest.cs
@@ -1,4 +1,5 @@
-﻿using BinaryKits.ZplUtility.Elements;
+﻿using BinaryKits.Zpl.Label;
+using BinaryKits.Zpl.Label.Elements;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Diagnostics;
