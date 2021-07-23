5ed3d450dc66820c10ef6453714d4624cda5d22d
Tino Hager <tino.hager@nager.at>
2021-07-23 21:05:01 +0200

    Optimize WebApi

---
+++ src/BinaryKits.Zpl.Viewer.WebApi/Models/ZplTestDataDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BinaryKits.Zpl.Viewer.WebApi.Models
+{
+    public class ZplTestDataDto
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
