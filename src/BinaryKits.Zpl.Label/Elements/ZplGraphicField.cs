6b7e0f2d7e79f24e9a4c226afe59fde0b5d5da7a
Tino Hager <tino.hager@nager.at>
2021-08-06 15:07:54 +0200

    Add GraphicField

---
+++ src/BinaryKits.Zpl.Label/Elements/ZplGraphicField.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryKits.Zpl.Label.Elements
+{
+    public class ZplGraphicField : ZplPositionedElementBase
+    {
+        public char CompressionType { get; private set; }
+        public int BinaryByteCount { get; private set; }
+        public int GraphicFieldCount { get; private set; }
+        public int BytesPerRow { get; private set; }
+        public string Data { get; private set; }
+
+        public ZplGraphicField(
+            int positionX,
+            int positionY,
+            int binaryByteCount,
+            int graphicFieldCount,
+            int bytesPerRow,
+            string data,
+            char compressionType = 'A')
+            : base(positionX, positionY)
+        {
+            this.CompressionType = compressionType;
+            this.BinaryByteCount = binaryByteCount;
+            this.GraphicFieldCount = graphicFieldCount;
+            this.BytesPerRow = bytesPerRow;
+            this.Data = data;
+        }
+
+        public override IEnumerable<string> Render(ZplRenderOptions context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/GraphicFieldZplCommandAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using BinaryKits.Zpl.Label.Elements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
+{
+    public class GraphicFieldZplCommandAnalyzer : ZplCommandAnalyzerBase
+    {
+        public GraphicFieldZplCommandAnalyzer(VirtualPrinter virtualPrinter) : base("^GF", virtualPrinter)
+        { }
+
+        public override ZplElementBase Analyze(string zplCommand)
+        {
+            var zplCommandData = zplCommand.Substring(this.PrinterCommandPrefix.Length);
+
+            var zplDataParts = zplCommandData.Split(',');
+
+
+
+            return null;
+        }
+    }
+}
