94cde8fa65f8084718eecada2aa87731ea9027a4
Tino Hager <tino.hager@nager.at>
2021-08-16 09:45:42 +0200

    Add logic for test prints

---
+++ src/BinaryKits.Zpl.Viewer.WebApi/Controllers/PrintController.cs
@@ -0,0 +1,39 @@
+﻿using BinaryKits.Zpl.Viewer.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Sockets;
+
+namespace BinaryKits.Zpl.Viewer.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PrintController : ControllerBase
+    {
+        private readonly ILogger<PrintController> _logger;
+
+        public PrintController(ILogger<PrintController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<RenderResponseDto> Render(PrintRequestDto request)
+        {
+            // Open connection
+            using var tcpClient = new TcpClient();
+            tcpClient.Connect(request.PrinterIpAddress, 9100);
+
+            // Send Zpl data to printer
+            using var writer = new System.IO.StreamWriter(tcpClient.GetStream());
+            writer.Write(request.ZplData);
+            writer.Flush();
+
+            // Close Connection
+            writer.Close();
+            tcpClient.Close();
+
+            return this.StatusCode(StatusCodes.Status200OK);
+        }
+    }
+}
---
+++ src/BinaryKits.Zpl.Viewer.WebApi/Models/PrintRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryKits.Zpl.Viewer.WebApi.Models
+{
+    public class PrintRequestDto
+    {
+        /// <summary>
+        /// Zpl data
+        /// </summary>
+        public string ZplData { get; set; }
+        /// <summary>
+        /// Printer IpAddress
+        /// </summary>
+        public string PrinterIpAddress { get; set; }
+
+    }
+}
