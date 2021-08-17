71c17e32261784658e4870cafe4772422364eddd
Tino Hager <tino.hager@nager.at>
2021-08-17 21:04:06 +0200

    Fix missing home position ^FO

--- src/BinaryKits.Zpl.Viewer/CommandAnalyzers/FieldOriginZplCommandAnalzer.cs
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/FieldOriginZplCommandAnalzer.cs
@@ -14,6 +14,12 @@ namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
             _ = int.TryParse(zplDataParts[0], out var x);
             _ = int.TryParse(zplDataParts[1], out var y);
 
+            if (this.VirtualPrinter.LabelHomePosition != null)
+            {
+                x += this.VirtualPrinter.LabelHomePosition.X;
+                y += this.VirtualPrinter.LabelHomePosition.Y;
+            }
+
             this.VirtualPrinter.SetNextElementPosition(x, y);
 
             return null;
