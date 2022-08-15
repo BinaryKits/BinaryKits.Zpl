945836f7d9cf54d877d13ca7460f5424d65e8460
primo-ppcg <mike.tryczak@gmail.com>
2022-08-15 19:33:00 +0700

    Add interpretation line to EAN13

--- src/BinaryKits.Zpl.Viewer/ElementDrawers/BarcodeEAN13ElementDrawer.cs
+++ src/BinaryKits.Zpl.Viewer/ElementDrawers/BarcodeEAN13ElementDrawer.cs
@@ -1,7 +1,9 @@
-﻿using BarcodeLib;
+using BarcodeLib;
 using BinaryKits.Zpl.Label.Elements;
+using BinaryKits.Zpl.Viewer.Helpers;
+using SkiaSharp;
+using System;
 using System.Drawing;
-
 
 namespace BinaryKits.Zpl.Viewer.ElementDrawers
 {
@@ -15,34 +17,51 @@ namespace BinaryKits.Zpl.Viewer.ElementDrawers
         ///<inheritdoc/>
         public override void Draw(ZplElementBase element)
         {
+            Draw(element, new DrawerOptions());
+        }
+
+        ///<inheritdoc/>
+        public override void Draw(ZplElementBase element, DrawerOptions options)
+        {
             if (element is ZplBarcodeEan13 barcode)
             {
                 float x = barcode.PositionX;
                 float y = barcode.PositionY;
 
-                if (barcode.FieldTypeset != null)
+                var content = barcode.Content;
+                content = content.PadLeft(12, '0').Substring(0, 12);
+                var interpretation = content;
+
+                if (barcode.PrintInterpretationLineAboveCode)
                 {
-                    y -= barcode.Height;
+                    int checksum = 0;
+                    for (int i = 0; i < 12; i++)
+                    {
+                        checksum += (content[i] - 48) * (9 - i % 2 * 2);
+                    }
+                    interpretation = string.Format("{0}{1}", interpretation, checksum % 10);
                 }
+
+                float labelFontSize = Math.Min(barcode.ModuleWidth * 7.2f, 72f);
+                var labelTypeFace = options.FontLoader("A");
+                var labelFont = new SKFont(labelTypeFace, labelFontSize).ToSystemDrawingFont();
+                int labelHeight = barcode.PrintInterpretationLine ? labelFont.Height : 0;
+                int labelHeightOffset = barcode.PrintInterpretationLineAboveCode ? labelHeight : 0;
 
                 var barcodeElement = new Barcode
                 {
                     BarWidth = barcode.ModuleWidth,
-                    BackColor = Color.Transparent,
-                    Height = barcode.Height                
+                    BackColor = Color.White,
+                    Height = barcode.Height + labelHeight,
+                    IncludeLabel = barcode.PrintInterpretationLine,
+                    LabelPosition = barcode.PrintInterpretationLineAboveCode ? LabelPositions.TOPCENTER : LabelPositions.BOTTOMCENTER,
+                    LabelFont = labelFont,
+                    AlternateLabel = interpretation,
+                    StandardizeLabel = !barcode.PrintInterpretationLineAboveCode
                 };
 
-                var content = barcode.Content;
-                if(content.Length < 12)
-                {
-                    var number = long.Parse(content);
-                    content = number.ToString("D12");
-                }else if(content.Length > 12)
-                {
-                    content = content.Substring(content.Length - 12, 12);
-                }
                 using var image = barcodeElement.Encode(TYPE.EAN13, content);
-                this.DrawBarcode(this.GetImageData(image), barcode.Height, image.Width, barcode.FieldOrigin != null, x, y, barcode.FieldOrientation);
+                this.DrawBarcode(this.GetImageData(image), barcode.Height, image.Width, barcode.FieldOrigin != null, x, y, labelHeightOffset, barcode.FieldOrientation);
             }
         }
     }
