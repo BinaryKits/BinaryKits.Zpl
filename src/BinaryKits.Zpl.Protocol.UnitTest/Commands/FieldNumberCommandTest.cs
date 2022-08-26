5518ebd1af4f79e3293e4b24b6c8b9709d8b0437
primo-ppcg <mike.tryczak@gmail.com>
2022-08-26 18:28:26 +0700

    Add Field Number

---
+++ src/BinaryKits.Zpl.Protocol.UnitTest/Commands/FieldNumberCommandTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryKits.Zpl.Protocol.Commands.UnitTest
+{
+    [TestClass]
+    public class FieldNumberCommandTest
+    {
+        [TestMethod]
+        public void ToZpl_Default_Successful()
+        {
+            var command = new FieldNumberCommand();
+            var zplCommand = command.ToZpl();
+            Assert.AreEqual("^FN0", zplCommand);
+        }
+
+        [TestMethod]
+        public void ToZpl_DefaultWithNumber_Successful()
+        {
+            var command = new FieldNumberCommand(2);
+            var zplCommand = command.ToZpl();
+            Assert.AreEqual("^FN2", zplCommand);
+        }
+
+        [TestMethod]
+        public void IsCommandParsable_ValidCommand_True()
+        {
+            var isParsable = FieldNumberCommand.CanParseCommand("^FN2\"Test\"");
+            Assert.IsTrue(isParsable);
+        }
+
+        [TestMethod]
+        public void IsCommandParsable_InvalidCommand_False()
+        {
+            var isParsable = FieldNumberCommand.CanParseCommand("^FT10,10");
+            Assert.IsFalse(isParsable);
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand1_Successful()
+        {
+            var command = CommandBase.ParseCommand("^FN");
+            Assert.IsTrue(command is FieldNumberCommand);
+            if (command is FieldNumberCommand numberCommand)
+            {
+                Assert.AreEqual(0, numberCommand.AssignedNumber);
+                Assert.AreEqual("optional parameter", numberCommand.PromptDisplay);
+            }
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand2_Successful()
+        {
+            var command = CommandBase.ParseCommand("^FN\"test\"");
+            Assert.IsTrue(command is FieldNumberCommand);
+            if (command is FieldNumberCommand numberCommand)
+            {
+                Assert.AreEqual(0, numberCommand.AssignedNumber);
+                Assert.AreEqual("test", numberCommand.PromptDisplay);
+            }
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand3_Successful()
+        {
+            var command = CommandBase.ParseCommand("^FN2");
+            Assert.IsTrue(command is FieldNumberCommand);
+            if (command is FieldNumberCommand numberCommand)
+            {
+                Assert.AreEqual(2, numberCommand.AssignedNumber);
+                Assert.AreEqual("optional parameter", numberCommand.PromptDisplay);
+            }
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand4_Successful()
+        {
+            var command = CommandBase.ParseCommand("^FN9999\"enter a number\"");
+            Assert.IsTrue(command is FieldNumberCommand);
+            if (command is FieldNumberCommand numberCommand)
+            {
+                Assert.AreEqual(9999, numberCommand.AssignedNumber);
+                Assert.AreEqual("enter a number", numberCommand.PromptDisplay);
+            }
+        }
+
+    }
+}
---
+++ src/BinaryKits.Zpl.Protocol/Commands/FieldNumberCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BinaryKits.Zpl.Protocol.Commands
+{
+    public class FieldNumberCommand : CommandBase
+    {
+        ///<inheritdoc/>
+        protected static new readonly string CommandPrefix = "^FN";
+
+        /// <summary>
+        /// Number to be assigned to the field. Accepted Values: 0 to 9999
+        /// </summary>
+        public int AssignedNumber { get; private set; } = 0;
+
+        /// <summary>
+        /// The optional prompt display parameter can be used with the KDU Plus to cause prompts to be displayed
+        /// on the KDU unit. Also, when the Print on Label link is selected on the Directory page of
+        /// ZebraLink enabled printers the field prompt displays.
+        /// </summary>
+        public string PromptDisplay { get; private set; } = "optional parameter";
+
+        /// <summary>
+        /// Field Separator
+        /// </summary>
+        public FieldNumberCommand()
+        { }
+
+        /// <summary>
+        /// Field Separator
+        /// </summary>
+        /// <param name="assignedNumber">Number to be assigned to the field</param>
+        /// <param name="promptDisplay">Prompt display (ignored)</param>
+        public FieldNumberCommand(int assignedNumber, string promptDisplay = "optional parameter")
+        {
+            this.AssignedNumber = assignedNumber;
+            this.PromptDisplay = promptDisplay;
+        }
+
+        ///<inheritdoc/>
+        public override string ToZpl()
+        {
+            // NB. PromptDisplay is ignored
+            return $"{CommandPrefix}{this.AssignedNumber}";
+        }
+
+        ///<inheritdoc/>
+        public static new bool CanParseCommand(string zplCommand)
+        {
+            return zplCommand.StartsWith(CommandPrefix, StringComparison.OrdinalIgnoreCase);
+        }
+
+        ///<inheritdoc/>
+        public static new CommandBase ParseCommand(string zplCommand)
+        {
+            var command = new FieldNumberCommand();
+            var zplCommandData = zplCommand.Substring(CommandPrefix.Length);
+            var fieldNumberDataMatch = Regex.Match(zplCommandData, @"^(\d+)?(?:""([a-zA-Z1-9 ]*)"")?$");
+
+            if (fieldNumberDataMatch.Success)
+            {
+                if (fieldNumberDataMatch.Groups[1].Success)
+                {
+                    command.AssignedNumber = int.Parse(fieldNumberDataMatch.Groups[1].Value);
+                }
+                if (fieldNumberDataMatch.Groups[2].Success)
+                {
+                    command.PromptDisplay = fieldNumberDataMatch.Groups[2].Value;
+                }
+            }
+
+            return command;
+        }
+
+    }
+}
