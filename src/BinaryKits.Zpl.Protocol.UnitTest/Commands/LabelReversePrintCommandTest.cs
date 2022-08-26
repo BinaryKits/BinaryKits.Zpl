c353cfbe263e2a66d87d625fe796564186a83744
primo-ppcg <mike.tryczak@gmail.com>
2022-08-26 16:19:22 +0700

    Add Label Reverse Print

---
+++ src/BinaryKits.Zpl.Protocol.UnitTest/Commands/LabelReversePrintCommandTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryKits.Zpl.Protocol.Commands.UnitTest
+{
+    [TestClass]
+    public class LabelReversePrintCommandTest
+    {
+        [TestMethod]
+        public void ToZpl_Default1_Successful()
+        {
+            var command = new LabelReversePrintCommand();
+            var zplCommand = command.ToZpl();
+            Assert.AreEqual("^LRN", zplCommand);
+        }
+
+        [TestMethod]
+        public void ToZpl_Default2_Successful()
+        {
+            var command = new LabelReversePrintCommand(true);
+            var zplCommand = command.ToZpl();
+            Assert.AreEqual("^LRY", zplCommand);
+        }
+
+        [TestMethod]
+        public void IsCommandParsable_ValidCommand_True()
+        {
+            var isParsable = LabelReversePrintCommand.CanParseCommand("^LRY");
+            Assert.IsTrue(isParsable);
+        }
+
+        [TestMethod]
+        public void IsCommandParsable_InvalidCommand_False()
+        {
+            var isParsable = LabelReversePrintCommand.CanParseCommand("^FT10,10");
+            Assert.IsFalse(isParsable);
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand1_Successful()
+        {
+            var command = CommandBase.ParseCommand("^LR");
+            Assert.IsTrue(command is LabelReversePrintCommand);
+            if (command is LabelReversePrintCommand reversePrintCommand)
+            {
+                Assert.IsFalse(reversePrintCommand.ReversePrintAllFields);
+            }
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand2_Successful()
+        {
+            var command = CommandBase.ParseCommand("^LRN");
+            Assert.IsTrue(command is LabelReversePrintCommand);
+            if (command is LabelReversePrintCommand reversePrintCommand)
+            {
+                Assert.IsFalse(reversePrintCommand.ReversePrintAllFields);
+            }
+        }
+
+        [TestMethod]
+        public void ParseCommand_ValidCommand3_Successful()
+        {
+            var command = CommandBase.ParseCommand("^LRY");
+            Assert.IsTrue(command is LabelReversePrintCommand);
+            if (command is LabelReversePrintCommand reversePrintCommand)
+            {
+                Assert.IsTrue(reversePrintCommand.ReversePrintAllFields);
+            }
+        }
+
+    }
+}
---
+++ src/BinaryKits.Zpl.Protocol/Commands/LabelReversePrintCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BinaryKits.Zpl.Protocol.Commands
+{
+    /// <summary>
+    /// Label Reverse Print<br/>
+    /// The ^LR command reverses the printing of all fields in the label format. It
+    /// allows a field to appear as white over black or black over white.
+    /// Using the ^LR is identical to placing an ^FR command in all current and subsequent fields.
+    /// </summary>
+    public class LabelReversePrintCommand : CommandBase
+    {
+        ///<inheritdoc/>
+        protected static new readonly string CommandPrefix = "^LR";
+
+        /// <summary>
+        /// Reverse print all fields
+        /// </summary>
+        public bool ReversePrintAllFields { get; private set; } = false;
+
+        /// <summary>
+        /// Label Reverse Print
+        /// </summary>
+        public LabelReversePrintCommand()
+        { }
+
+        /// <summary>
+        /// Label Reverse Print
+        /// </summary>
+        /// <param name="reversePrintAllFields">Reverse print all fields</param>
+        public LabelReversePrintCommand(bool reversePrintAllFields)
+        {
+            this.ReversePrintAllFields = reversePrintAllFields;
+        }
+
+        ///<inheritdoc/>
+        public override string ToZpl()
+        {
+            return $"{CommandPrefix}{RenderBoolean(this.ReversePrintAllFields)}";
+        }
+
+        ///<inheritdoc/>
+        public static new bool CanParseCommand(string zplCommand)
+        {
+            return zplCommand.StartsWith(CommandPrefix, StringComparison.OrdinalIgnoreCase);
+        }
+
+        ///<inheritdoc/>
+        public static new CommandBase ParseCommand(string zplCommand)
+        {
+            var command = new LabelReversePrintCommand();
+            var zplCommandData = zplCommand.Substring(CommandPrefix.Length);
+
+            command.ReversePrintAllFields = ConvertBoolean(zplCommandData);
+
+            return command;
+        }
+
+    }
+}
