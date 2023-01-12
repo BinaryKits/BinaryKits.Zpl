4072c3b166d8e02f3e42c74c52c8d3325185df02
primo-ppcg <mike.tryczak@gmail.com>
2023-01-12 13:32:30 +0700

    Parse DeviceName and Extension for DownloadFormat
    Fixes #150

--- src/BinaryKits.Zpl.Viewer/CommandAnalyzers/DownloadFormatCommandAnalyzer.cs
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/DownloadFormatCommandAnalyzer.cs
@@ -1,17 +1,26 @@
 ﻿using BinaryKits.Zpl.Label.Elements;
+using System.Text.RegularExpressions;
 
 namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
 {
     public class DownloadFormatCommandAnalyzer : ZplCommandAnalyzerBase
     {
+        private static readonly Regex commandRegex = new Regex(@"^\^DF(\w:)?(.*?)?(\..+?)?$", RegexOptions.Compiled);
+
         public DownloadFormatCommandAnalyzer(VirtualPrinter virtualPrinter) : base("^DF", virtualPrinter)
         { }
 
         public override ZplElementBase Analyze(string zplCommand)
         {
-            var name = zplCommand.Substring(this.PrinterCommandPrefix.Length);
+            var commandMatch = commandRegex.Match(zplCommand);
+            if (commandMatch.Success)
+            {
+                var storageDevice = commandMatch.Groups[1].Success ? commandMatch.Groups[1].Value[0] : 'R';
+                var formatName = commandMatch.Groups[2].Value;
 
-            this.VirtualPrinter.SetNextDownloadFormatName(name);
+                this.VirtualPrinter.SetNextDownloadFormatName(formatName);
+            }
+
             return null;
         }
     }
