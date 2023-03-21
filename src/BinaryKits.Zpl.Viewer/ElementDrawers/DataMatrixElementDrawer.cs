8e4615d7ac929dd5f56f41e522ed5227b2228b81
Guilherme Manucci <T1006497@marfrig.com.br>
2023-03-21 09:31:04 -0300

    fix: returned specific information when Matrix Barcode has no content nor Height

--- src/BinaryKits.Zpl.Viewer/ElementDrawers/DataMatrixElementDrawer.cs
+++ src/BinaryKits.Zpl.Viewer/ElementDrawers/DataMatrixElementDrawer.cs
@@ -20,6 +20,12 @@ namespace BinaryKits.Zpl.Viewer.ElementDrawers
         {
             if (element is ZplDataMatrix dataMatrix)
             {
+                if (dataMatrix.Height == 0)
+                    throw new System.Exception("Matrix Height is set to zero.");
+
+                if (string.IsNullOrWhiteSpace(dataMatrix.Content))
+                    throw new System.Exception("Matrix Content is empty.");
+
                 float x = dataMatrix.PositionX;
                 float y = dataMatrix.PositionY;
 
@@ -30,9 +36,10 @@ namespace BinaryKits.Zpl.Viewer.ElementDrawers
                 var result = writer.encode(dataMatrix.Content, BarcodeFormat.DATA_MATRIX, 0, 0, hints);
 
                 using var resizedImage = this.BitMatrixToSKBitmap(result, dataMatrix.Height);
-
-                var png = resizedImage.Encode(SKEncodedImageFormat.Png, 100).ToArray();
-                this.DrawBarcode(png, resizedImage.Height, resizedImage.Width, dataMatrix.FieldOrigin != null, x, y, 0, dataMatrix.FieldOrientation);
+                {
+                    var png = resizedImage.Encode(SKEncodedImageFormat.Png, 100).ToArray();
+                    this.DrawBarcode(png, resizedImage.Height, resizedImage.Width, dataMatrix.FieldOrigin != null, x, y, 0, dataMatrix.FieldOrientation);
+                }
             }
         }
     }
