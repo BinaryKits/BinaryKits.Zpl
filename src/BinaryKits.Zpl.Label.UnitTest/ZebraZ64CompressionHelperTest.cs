408c0984e15813902973abe8b9184cdd2a85707b
primo-ppcg <mike.tryczak@gmail.com>
2023-06-21 18:30:39 +0700

    Unify compression output across platform
    Both libraries agree on the output of `CompressionLevel.SmallestSize`.

--- src/BinaryKits.Zpl.Label.UnitTest/ZebraZ64CompressionHelperTest.cs
+++ src/BinaryKits.Zpl.Label.UnitTest/ZebraZ64CompressionHelperTest.cs
@@ -12,7 +12,7 @@ namespace BinaryKits.Zpl.Label.UnitTest
         public void Compress_ValidData1_Successful()
         {
             var compressed = ZebraZ64CompressionHelper.Compress("FFFFFFFFFFFFFFFFFFFF8000FFFF0000FFFF00018000FFFF0000FFFF00018000FFFF0000FFFF0001FFFF0000FFFF0000FFFFFFFF0000FFFF0000FFFFFFFF0000FFFF0000FFFFFFFFFFFFFFFFFFFFFFFF");
-            Assert.AreEqual(":Z64:eJz7/x8GGhj+/2cAYUZsLCgNxNhZMAAA4sMzUg==:f3e9", compressed);
+            Assert.AreEqual(":Z64:eNr7/x8GGhj+/2cAYUZsLCjNAFKJjQUDAOLDM1I=:8f39", compressed);
             //^XA
             //~DGR:SAMPLE.GRF,00006,02,JFGFH0GFJF
             //^FO20,20
