30067766254448b3d0d011ec6f08d68dbeff0b47
primo-ppcg <mike.tryczak@gmail.com>
2024-05-20 15:45:46 +0700

    Implement `^FW`

---
+++ src/BinaryKits.Zpl.Label/Elements/ZplFieldOrientation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BinaryKits.Zpl.Label.Elements
+{
+    /// <summary>
+    /// ^FW - Field Orientation
+    /// </summary>
+    public class ZplFieldOrientation : ZplElementBase
+    {
+        public FieldOrientation FieldOrientation { get; private set; }
+
+        public ZplFieldOrientation(FieldOrientation fieldOrientation)
+        {
+            this.FieldOrientation = fieldOrientation;
+        }
+
+        ///<inheritdoc/>
+        public override IEnumerable<string> Render(ZplRenderOptions context)
+        {
+            return new[] { $"^FW{RenderFieldOrientation(this.FieldOrientation)}" };
+        }
+
+    }
+}
---
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/FieldOrientationZplCommandAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using BinaryKits.Zpl.Label.Elements;
+
+namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
+{
+    public class FieldOrientationZplCommandAnalyzer : ZplCommandAnalyzerBase
+    {
+        public FieldOrientationZplCommandAnalyzer(VirtualPrinter virtualPrinter) : base("^FW", virtualPrinter) { }
+
+        ///<inheritdoc/>
+        public override ZplElementBase Analyze(string zplCommand)
+        {
+            var zplDataParts = this.SplitCommand(zplCommand);
+            if (zplDataParts.Length > 0)
+            {
+                var fieldOrientation = ConvertFieldOrientation(zplDataParts[0]);
+                this.VirtualPrinter.SetFieldOrientation(fieldOrientation);
+            }
+
+            return null;
+        }
+    }
+}
