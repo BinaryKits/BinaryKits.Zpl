492c42bc08840a99cd4356f2a694b4b2e59be88d
Alex Rousseaux <alexr@peacocks.com.au>
2024-09-04 12:05:50 +1000

    Add quality level to DataMatrix barcodes.

--- src/BinaryKits.Zpl.Label/Elements/ZplDataMatrix.cs
+++ src/BinaryKits.Zpl.Label/Elements/ZplDataMatrix.cs
@@ -16,30 +16,40 @@ namespace BinaryKits.Zpl.Label.Elements
         /// <param name="height"></param>
         /// <param name="fieldOrientation"></param>
         /// <param name="bottomToTop"></param>
+        /// <param name="qualityLevel"></param>
         public ZplDataMatrix(
             string content,
             int positionX,
             int positionY,
             int height = 100,
             FieldOrientation fieldOrientation = FieldOrientation.Normal,
-            bool bottomToTop = false
+            bool bottomToTop = false,
+            QualityLevel qualityLevel = QualityLevel.ECC0
            )
             : base(positionX, positionY, bottomToTop)
         {
             Content = content;
             FieldOrientation = fieldOrientation;
             Height = height;
+            QualityLevel = qualityLevel;
         }
 
         public int Height { get; protected set; }
 
         public FieldOrientation FieldOrientation { get; protected set; }
 
+        public QualityLevel QualityLevel { get; protected set; }
+
         public string Content { get; protected set; }
 
         protected string RenderFieldOrientation()
         {
             return RenderFieldOrientation(FieldOrientation);
+        }
+
+        protected string RenderQualityLevel()
+        {
+            return RenderQualityLevel(QualityLevel);
         }
 
         ///<inheritdoc/>
@@ -50,7 +60,7 @@ namespace BinaryKits.Zpl.Label.Elements
             //^FDZEBRA TECHNOLOGIES CORPORATION ^ FS
             var result = new List<string>();
             result.AddRange(RenderPosition(context));
-            result.Add($"^BX{RenderFieldOrientation()},{context.Scale(Height)}");
+            result.Add($"^BX{RenderFieldOrientation()},{context.Scale(Height)},{RenderQualityLevel()}");
             result.Add($"^FD{Content}^FS");
 
             return result;
