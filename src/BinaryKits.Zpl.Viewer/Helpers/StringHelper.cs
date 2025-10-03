c962ea97e1f0202e688e8882d9dd625ed9eea320
primo-ppcg <mike.tryczak@gmail.com>
2025-10-03 10:40:24 +0700

    Parse UTF8 in Hex Escapes

--- src/BinaryKits.Zpl.Viewer/Helpers/StringHelper.cs
+++ src/BinaryKits.Zpl.Viewer/Helpers/StringHelper.cs
@@ -1,11 +1,14 @@
-using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace BinaryKits.Zpl.Viewer.Helpers
 {
     public static class StringHelper
     {
+        private static readonly Regex hexDigits = new Regex(@"^[0-9A-Fa-f]{2}$", RegexOptions.Compiled);
+
         /// <summary>
         /// The hexadecimal indicator from the ^FH
         /// </summary>
@@ -18,8 +21,25 @@ namespace BinaryKits.Zpl.Viewer.Helpers
         /// <returns>Text with hex escapes replaced with their char equivalents.</returns>
         public static string ReplaceHexEscapes(this string text)
         {
-            Regex hexEscapeRegex = new Regex(Regex.Escape(ReplaceChar.ToString()) + @"([0-9A-Fa-f]{2})");
-            return hexEscapeRegex.Replace(text, match => Convert.ToChar(int.Parse(match.Groups[1].Value, NumberStyles.HexNumber)).ToString());
+            List<byte> bytes = new List<byte>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (c == ReplaceChar && i + 2 < text.Length)
+                {
+                    string digits = text.Substring(i + 1, 2);
+                    if (hexDigits.IsMatch(digits))
+                    {
+                        bytes.Add(byte.Parse(digits, NumberStyles.HexNumber));
+                        i += 2;
+                        continue;
+                    }
+                }
+
+                bytes.Add((byte)c);
+            }
+
+            return Encoding.UTF8.GetString(bytes.ToArray());
         }
     }
 }
