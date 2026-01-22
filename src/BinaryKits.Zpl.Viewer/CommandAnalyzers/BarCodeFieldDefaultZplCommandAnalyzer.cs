d3bc134602e499381756240b85b935a56fc929ab
Matteo Dell'Acqua <matteodellacqua@ibilab.it>
2026-01-22 15:28:09 +0100

    Do not override barcode default if not specified

--- src/BinaryKits.Zpl.Viewer/CommandAnalyzers/BarCodeFieldDefaultZplCommandAnalyzer.cs
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/BarCodeFieldDefaultZplCommandAnalyzer.cs
@@ -13,28 +13,21 @@ namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
 
             int tmpint;
             double tmpdbl;
-            int moduleWidth = 2;
-            double wideBarToNarrowBarWidthRatio = 3.0;
-            int barcodeHeight = 10;
 
             if (zplDataParts.Length > 0 && int.TryParse(zplDataParts[0], out tmpint))
             {
-                moduleWidth = tmpint;
+                virtualPrinter.SetBarcodeModuleWidth(tmpint);
             }
 
             if (zplDataParts.Length > 1 && double.TryParse(zplDataParts[1], out tmpdbl))
             {
-                wideBarToNarrowBarWidthRatio = tmpdbl;
+                virtualPrinter.SetBarcodeWideBarToNarrowBarWidthRatio(tmpdbl);
             }
 
             if (zplDataParts.Length > 2 && int.TryParse(zplDataParts[2], out tmpint))
             {
-                barcodeHeight = tmpint;
+                virtualPrinter.SetBarcodeHeight(tmpint);
             }
-
-            virtualPrinter.SetBarcodeModuleWidth(moduleWidth);
-            virtualPrinter.SetBarcodeWideBarToNarrowBarWidthRatio(wideBarToNarrowBarWidthRatio);
-            virtualPrinter.SetBarcodeHeight(barcodeHeight);
 
             return null;
         }
