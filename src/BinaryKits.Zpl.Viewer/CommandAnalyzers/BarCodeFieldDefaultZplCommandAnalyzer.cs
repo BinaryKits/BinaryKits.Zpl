de28e77218318255f6037a74c21b64caa68da3aa
Matteo Dell'Acqua <matteodellacqua@ibilab.it>
2026-01-22 15:28:09 +0100

    Add validation to ^BY

--- src/BinaryKits.Zpl.Viewer/CommandAnalyzers/BarCodeFieldDefaultZplCommandAnalyzer.cs
+++ src/BinaryKits.Zpl.Viewer/CommandAnalyzers/BarCodeFieldDefaultZplCommandAnalyzer.cs
@@ -16,16 +16,46 @@ namespace BinaryKits.Zpl.Viewer.CommandAnalyzers
 
             if (zplDataParts.Length > 0 && int.TryParse(zplDataParts[0], out tmpint))
             {
+                // TODO: add validation message: between 1 and 10
+                if (tmpint < 1)
+                {
+                    tmpint = 1;
+                }
+                else if (tmpint > 10)
+                {
+                    tmpint = 10;
+                }
+
                 virtualPrinter.SetBarcodeModuleWidth(tmpint);
             }
 
             if (zplDataParts.Length > 1 && double.TryParse(zplDataParts[1], out tmpdbl))
             {
+                // TODO: add validation message: between 2.0 and 3.0 in 0.1 increments
+                if (tmpdbl < 2.0)
+                {
+                    tmpdbl = 2.0;
+                }
+                else if (tmpdbl > 3.0)
+                {
+                    tmpdbl = 3.0;
+                }
+                else
+                {
+                    tmpdbl = System.Math.Round(tmpdbl, 1);
+                }
+
                 virtualPrinter.SetBarcodeWideBarToNarrowBarWidthRatio(tmpdbl);
             }
 
             if (zplDataParts.Length > 2 && int.TryParse(zplDataParts[2], out tmpint))
             {
+                // TODO: add validation message: greater or equal than 1
+                if (tmpint < 1)
+                {
+                    tmpint = 1;
+                }
+
                 virtualPrinter.SetBarcodeHeight(tmpint);
             }
 
